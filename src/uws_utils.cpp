b740592db3b6f7f0b39203dcb69205bf6a6dc931
Daniel M Campos <daniel.m.campos@icloud.com>
2021-04-23 13:39:46 -0500

    Refactor uws_utils
    - Add Handler interface used by Simulation to encapsulate iterating with
    the game simulator
    - Rename handler alias
    - Add CreateMainHandler that takes a Controller
    - Add CreateMessage that takes a Controller

--- src/uws_utils.cpp
+++ src/uws_utils.cpp
@@ -3,6 +3,7 @@
 #include "parser.h"
 
 void uws_utils::ResetSimulator(uWS::WebSocket<uWS::SERVER> ws) {
+  std::cout << "Resetting Simulator\n";
   std::string reset_msg = "42[\"reset\",{}]";
   ws.send(reset_msg.data(), reset_msg.length(), uWS::OpCode::TEXT);
 }
@@ -15,7 +16,7 @@ std::unique_ptr<uWS::Hub> uws_utils::CreateHub(int port) {
   hub->onDisconnection([](uWS::WebSocket<uWS::SERVER> ws, int code,
                           char *message, size_t length) {
     ws.close();
-    std::cout << "Disconnected" << std::endl;
+    std::cout << "Disconnected\n";
   });
   if (hub->listen(port)) {
     std::cout << "Listening to port " << port << std::endl;
@@ -26,7 +27,7 @@ std::unique_ptr<uWS::Hub> uws_utils::CreateHub(int port) {
   return hub;
 }
 
-uws_utils::Handler uws_utils::CreateMainHandler(PID &steer_pid,
+uws_utils::handler uws_utils::CreateMainHandler(PID &steer_pid,
                                                 double throttle) {
   using namespace internal;
   return [&steer_pid, throttle](uWS::WebSocket<uWS::SERVER> ws, char *data,
@@ -51,14 +52,37 @@ uws_utils::Handler uws_utils::CreateMainHandler(PID &steer_pid,
   };
 }
 
-uws_utils::Handler uws_utils::CreateSimulationHandler(PID &steer_pid,
+uws_utils::handler uws_utils::CreateMainHandler(Controller &controller) {
+  using namespace internal;
+  return [&controller](uWS::WebSocket<uWS::SERVER> ws, char *data,
+                       size_t length, uWS::OpCode opCode) {
+    if (IsMessageEvent(length, data)) {
+      auto s = parser::HasData(std::string(data).substr(0, length));
+      if (!s.empty()) {
+        auto j = json::parse(s);
+        auto event = j[0].get<std::string>();
+        if (event == "telemetry") {
+          auto msg = CreateMessage(parser::ParseMessage(j[1]), controller);
+          std::cout << msg << std::endl;
+          ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+        }
+      } else {
+        // Manual driving
+        std::string msg = "42[\"manual\",{}]";
+        ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+      }
+    }
+  };
+}
+uws_utils::handler uws_utils::CreateSimulationHandler(PID &steer_pid,
                                                       size_t max_steps,
                                                       double throttle) {
   using namespace internal;
   size_t counter = 0;
-  return [&steer_pid, &max_steps, &counter, throttle](
+  return [&steer_pid, &max_steps, counter, throttle](
              uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-             uWS::OpCode opCode) {
+             uWS::OpCode opCode) mutable {
+    std::cout << "counter=" << counter << std::endl;
     if (counter >= max_steps) {
       ResetSimulator(ws);
       return;
@@ -81,7 +105,8 @@ bool uws_utils::internal::IsMessageEvent(size_t length, const char *data) {
 }
 
 std::string uws_utils::internal::CreateMessage(SensorData sensor_data,
-                                               PID steer_pid, double throttle) {
+                                               PID &steer_pid,
+                                               double throttle) {
   std::cout << sensor_data << std::endl;
   steer_pid.UpdateError(sensor_data.cross_track_error);
   auto error = steer_pid.TotalError();
@@ -92,4 +117,50 @@ std::string uws_utils::internal::CreateMessage(SensorData sensor_data,
   auto msg = "42[\"steer\"," + msgJson.dump() + "]";
   std::cout << msg << std::endl;
   return msg;
+}
+
+std::string uws_utils::internal::CreateMessage(SensorData sensor_data,
+                                               Controller &controller) {
+  std::cout << sensor_data << std::endl;
+  auto controls = controller(sensor_data);
+  json msgJson;
+  msgJson["steering_angle"] = controls.steering_angle;
+  msgJson["throttle"] = controls.throttle;
+  auto msg = "42[\"steer\"," + msgJson.dump() + "]";
+  std::cout << msg << std::endl;
+  return msg;
+}
+
+uws_utils::Simulation::Simulation(PID &steer_pid, size_t max_steps,
+                                  double throttle)
+    : steer_pid_{steer_pid},
+      max_steps_{max_steps},
+      throttle_{throttle},
+      counter_{0} {}
+
+void uws_utils::Simulation::operator()(uWS::WebSocket<uWS::SERVER> ws,
+                                       char *data, size_t length,
+                                       uWS::OpCode code) {
+  using namespace internal;
+  std::cout << "counter=" << counter_ << std::endl;
+  if (counter_ >= max_steps_) {
+    counter_ = 0;
+    ResetSimulator(ws);
+    throw UWSUtilsException();
+  }
+  if (IsMessageEvent(length, data)) {
+    auto s = parser::HasData(std::string(data).substr(0, length));
+    if (s.empty()) return;
+    json j;
+    try {
+      j = json::parse(s);
+    } catch (const std::invalid_argument &e) {
+      return;
+    }
+    auto event = j[0].get<std::string>();
+    if (event != "telemetry") return;
+    auto msg = CreateMessage(parser::ParseMessage(j[1]), steer_pid_, throttle_);
+    ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
+    ++counter_;
+  }
 }
--- src/uws_utils.h
+++ src/uws_utils.h
@@ -3,28 +3,58 @@
 
 #include <uWS/uWS.h>
 
+#include "controller.h"
 #include "datamodels.h"
 #include "pid.h"
 
 namespace uws_utils {
-using Handler = std::function<void(uWS::WebSocket<uWS::SERVER>, char*, size_t,
+
+class Handler {
+ public:
+  virtual ~Handler() = default;
+  virtual void operator()(uWS::WebSocket<uWS::SERVER>, char*, size_t,
+                          uWS::OpCode) = 0;
+};
+
+class Simulation : public Handler {
+ public:
+  Simulation(PID& steer_pid, size_t max_steps, double throttle = 1.0);
+  void operator()(uWS::WebSocket<uWS::SERVER> ws, char* data, size_t length,
+                  uWS::OpCode code) override;
+
+ private:
+  PID& steer_pid_;
+  size_t max_steps_;
+  double throttle_;
+  int counter_;
+};
+
+struct UWSUtilsException : public std::exception {
+  const char* what() const throw() { return "uws_utils exception"; }
+};
+
+using handler = std::function<void(uWS::WebSocket<uWS::SERVER>, char*, size_t,
                                    uWS::OpCode)>;
 
 std::unique_ptr<uWS::Hub> CreateHub(int port);
 
 void ResetSimulator(uWS::WebSocket<uWS::SERVER> ws);
 
-Handler CreateMainHandler(PID& steer_pid, double throttle = 0.3);
+handler CreateMainHandler(PID& steer_pid, double throttle = 0.3);
 
-Handler CreateSimulationHandler(PID& steer_pid, size_t max_steps,
+handler CreateMainHandler(Controller& controller);
+
+handler CreateSimulationHandler(PID& steer_pid, size_t max_steps,
                                 double throttle = 0.3);
 
 namespace internal {
 
 bool IsMessageEvent(size_t length, const char* data);
 
-std::string CreateMessage(SensorData sensor_data, PID steer_pid,
+std::string CreateMessage(SensorData sensor_data, PID& steer_pid,
                           double throttle);
+
+std::string CreateMessage(SensorData sensor_data, Controller& controller);
 
 }  // namespace internal
 
