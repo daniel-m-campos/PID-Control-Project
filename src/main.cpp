546a73ae3a5940e2387a89f1d330f1bcc3b2173c
Daniel M Campos <daniel.m.campos@icloud.com>
2021-04-23 13:40:25 -0500

    Use a Controller in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 
+#include "controller.h"
 #include "json.hpp"
 #include "pid.h"
 #include "uws_utils.h"
@@ -10,8 +11,10 @@ using std::string;
 constexpr int kPort = 4567;
 
 int main() {
-  PID pid(0.01, 0.0, 0.0);
+  PID steer_pid(0.1, 0.0, 4.0);
+  PID throttle_pid(0.2, 0.0, 3.0);
+  Controller controller(steer_pid, throttle_pid, 200.0, 0.25);
   auto hub = uws_utils::CreateHub(kPort);
-  hub->onMessage(uws_utils::CreateMainHandler(pid));
+  hub->onMessage(uws_utils::CreateMainHandler(controller));
   hub->run();
 }
